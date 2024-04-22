50bf1c49e0c1a25fdf9604b71bb02d0f4b868119
Дмитрий Пашков <86959216+DmitryPash@users.noreply.github.com>
2024-04-22 10:50:39 +0300

    back forvard

--- script.js
+++ script.js
@@ -1,33 +1,118 @@
 const D = document;
+const main = D.querySelector(".main");
+const man = D.querySelector(".man");
+const BODY = D.querySelector("body");
+const sections = D.querySelectorAll(".section");
 
-const man = D.querySelector('.man')
-const BODY = D.querySelector('body')
-const background = D.querySelector('.background')
+// Маркер для сравнения место нахождения
 let positionX = 0;
+const DATA = {
+  0: "Дальше пути нету",
+  1: null,
+  2: `<div class='man__dialog'>Меня зовут @name</div>`,
+  3: null,
+  4: `<div class='man__zxc'>zxczxc</div>`,
+  5: null,
+  6: null,
+  7: null,
+  8: null,
+  9: null,
+  10: "Когда пройдет 60 секунд",
+  11: "Это значит, что прошла минута",
+};
 let stepSpeed = 0;
+//Позиция модельки заданная в свойсте left в css файле
+let startposition = Number(getComputedStyle(man).left.replace(/\D/g, ""));
+let manPosition = 0;
 let moveBg = 0;
-BODY.addEventListener('keydown', (key) => {
-    stepSpeed++
-    moveBg = moveBg + 2
-    if(stepSpeed >= 2) {
-        if(key.key === 'd' || key.key == 'D') {
-            if(positionX > 97) {
-                positionX = 0
-            } else {
-                console.log(stepSpeed)
-                positionX = positionX + 14.2
-                man.style.backgroundPositionX = `${positionX}%`
-                man.style.backgroundPositionY = 0
-                background.style.backgroundPositionX = `-${moveBg}px`
-            }
-        }
-        stepSpeed = 0
+BODY.addEventListener("keydown", (key) => {
+  stepSpeed += 10;
+
+  if (stepSpeed >= 2) {
+    if (key.key === "d" || key.key == "D" || key.key === "в" || key.key == "В") {
+        man.classList.remove('man-go--left')
+      if (positionX > 97) {
+        positionX = 0;
+      } else {
+        moveBg += 4;
+        manPosition = startposition + moveBg;
+        positionX = positionX + 14.2;
+        Object.assign(man.style, {
+          backgroundPositionX: `${positionX}%`,
+          backgroundPositionY: 0,
+          left: `${manPosition}px`,
+        });
+        main.scroll({
+          left: moveBg,
+          behavior: "smooth",
+        });
+        createBlock();
+      }
     }
-})
+    if(key.key === "a" || key.key == "A" || key.key === "ф" || key.key == "Ф") {
+        man.classList.add('man-go--left')
+        if(manPosition <= 0) {
+            return
+        }
+        if (positionX > 97) {
+            positionX = 0;
+          } else {
+            moveBg -= 4;
+            manPosition = startposition - (-moveBg);
+            positionX = positionX + 14.2;
+            Object.assign(man.style, {
+              backgroundPositionX: `${positionX}%`,
+              backgroundPositionY: 0,
+              left: `${manPosition}px`,
+            });
+            main.scroll({
+              left: moveBg,
+              behavior: "smooth",
+            });
+            createBlock();
+          }
+    }
+    stepSpeed = 0;
+  }
+});
 
-BODY.addEventListener('keyup', () => {
-    man.style.backgroundPositionX = `${42.6}%`
-    man.style.backgroundPositionY = `${100}%`
-    stepSpeed = 0
-})
+BODY.addEventListener("keyup", () => {
+  man.style.backgroundPositionX = `${42.6}%`;
+  man.style.backgroundPositionY = `${100}%`;
+  stepSpeed = 0;
+});
 
+// Секции, брекпоинты
+const colInSection = D.querySelectorAll(".section-col");
+colInSection.forEach((col, i) => {
+  col.setAttribute("data-object", i);
+});
+
+// Получаем id блока в которм таргет
+function getIdOfObject() {
+  const idObject = D.querySelectorAll(".section-col");
+
+  for (let i = 0; i < idObject.length; i++) {
+    if (
+      manPosition > idObject[i].offsetLeft &&
+      manPosition < idObject[i + 1].offsetLeft
+    ) {
+      return idObject[i].dataset.object;
+    }
+  }
+}
+
+// Создание диолога
+function createBlock() {
+  for (const [id, value] of Object.entries(DATA)) {
+    //Ищем совпадение в обьекте с id где находиться таргет и ключем объекта
+    if (id == getIdOfObject()) {
+      // Проверяем есть ли значение
+      if (value == null) {
+        man.innerHTML = "";
+      } else {
+        man.innerHTML = value;
+      }
+    }
+  }
+}
